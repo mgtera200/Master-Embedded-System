25b70e14361360ba0b01680f15543041f7ccdff3
tera <cool_moustafa_99@hotmail.com>
2023-09-29 21:18:51 +0300

    hi

---
+++ First-Term_Projects/Project.1/Project.1_Codes/alarm.h
@@ -0,0 +1,12 @@
+/*
+ * alarm.h
+ *
+ *  Created on: Sep 12, 2023
+ *      Author: MOHTERA
+ */
+
+#ifndef ALARM_H_
+#define ALARM_H_
+void (*Alarm_state)();
+extern void checking_signal ();
+#endif /* ALARM_H_ */

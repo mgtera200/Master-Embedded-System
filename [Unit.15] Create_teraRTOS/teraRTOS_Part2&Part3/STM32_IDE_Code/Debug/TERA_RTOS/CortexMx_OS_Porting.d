8f4d5106bc9334eb5a56b93a5dece67834f48317
tera <cool_moustafa_99@hotmail.com>
2023-12-26 03:47:33 +0200

    teraRTOS

---
+++ [Unit.15] Create_teraRTOS/teraRTOS_Part2&Part3/STM32_IDE_Code/Debug/TERA_RTOS/CortexMx_OS_Porting.d
@@ -0,0 +1,28 @@
+TERA_RTOS/CortexMx_OS_Porting.o: ../TERA_RTOS/CortexMx_OS_Porting.c \
+ ../TERA_RTOS/RTOS_inc/CortexMx_OS_Porting.h \
+ F:/Mostafa/Drivers/Drivers/CMSIS_V5/core_cm3.h \
+ F:/Mostafa/Drivers/Drivers/CMSIS_V5/ARMCM3.h \
+ F:/Mostafa/Drivers/Drivers/CMSIS_V5/core_cm3.h \
+ F:/Mostafa/Drivers/Drivers/CMSIS_V5/cmsis_version.h \
+ F:/Mostafa/Drivers/Drivers/CMSIS_V5/cmsis_compiler.h \
+ F:/Mostafa/Drivers/Drivers/CMSIS_V5/cmsis_gcc.h \
+ F:/Mostafa/Drivers/Drivers/CMSIS_V5/mpu_armv7.h \
+ F:/Mostafa/Drivers/Drivers/CMSIS_V5/system_ARMCM3.h
+
+../TERA_RTOS/RTOS_inc/CortexMx_OS_Porting.h:
+
+F:/Mostafa/Drivers/Drivers/CMSIS_V5/core_cm3.h:
+
+F:/Mostafa/Drivers/Drivers/CMSIS_V5/ARMCM3.h:
+
+F:/Mostafa/Drivers/Drivers/CMSIS_V5/core_cm3.h:
+
+F:/Mostafa/Drivers/Drivers/CMSIS_V5/cmsis_version.h:
+
+F:/Mostafa/Drivers/Drivers/CMSIS_V5/cmsis_compiler.h:
+
+F:/Mostafa/Drivers/Drivers/CMSIS_V5/cmsis_gcc.h:
+
+F:/Mostafa/Drivers/Drivers/CMSIS_V5/mpu_armv7.h:
+
+F:/Mostafa/Drivers/Drivers/CMSIS_V5/system_ARMCM3.h:

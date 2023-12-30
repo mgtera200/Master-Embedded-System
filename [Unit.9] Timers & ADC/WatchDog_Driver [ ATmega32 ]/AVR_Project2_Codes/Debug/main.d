f0c73ce54597762042f94ca84a5e70dc76ad58f2
tera <cool_moustafa_99@hotmail.com>
2023-12-30 18:53:35 +0200

    Timers_ADC

---
+++ [Unit.9] Timers & ADC/WatchDog_Driver [ ATmega32 ]/AVR_Project2_Codes/Debug/main.d
@@ -0,0 +1,7 @@
+main.o main.o: ../main.c \
+  C:\Users\Dell\eclipse-workspace\AVR_Project2\AVR_MCAL\inc/WatchDog.h \
+  C:\Users\Dell\eclipse-workspace\AVR_Project2\AVR_MCAL\inc/timer.h
+
+C:\Users\Dell\eclipse-workspace\AVR_Project2\AVR_MCAL\inc/WatchDog.h:
+
+C:\Users\Dell\eclipse-workspace\AVR_Project2\AVR_MCAL\inc/timer.h:

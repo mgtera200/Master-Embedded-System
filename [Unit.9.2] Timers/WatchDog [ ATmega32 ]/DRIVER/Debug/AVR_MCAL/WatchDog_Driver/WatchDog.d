44d17a89aade99045cbe948e2a0b9ff0a8f05421
TERA <127119775+mgtera200@users.noreply.github.com>
2024-02-26 09:43:48 +0200

    Delete [Unit.9.2] Timers directory

--- [Unit.9.2] Timers/WatchDog [ ATmega32 ]/DRIVER/Debug/AVR_MCAL/WatchDog_Driver/WatchDog.d
+++
@@ -1,5 +0,0 @@
-AVR_MCAL/WatchDog_Driver/WatchDog.o AVR_MCAL/WatchDog_Driver/WatchDog.o:  \
- ../AVR_MCAL/WatchDog_Driver/WatchDog.c \
-  C:\Users\Dell\eclipse-workspace\AVR_Project2\AVR_MCAL\inc/WatchDog.h
-
-C:\Users\Dell\eclipse-workspace\AVR_Project2\AVR_MCAL\inc/WatchDog.h:

c09be5f2de0efe9990647de622065f2b6bb224bf
TERA <127119775+mgtera200@users.noreply.github.com>
2024-02-26 04:23:50 +0200

    Delete [Unit.9] Timers & ADC directory

--- [Unit.9] Timers & ADC/ADC_Driver [ ATmega32 ]/README.md
+++
@@ -1,4 +0,0 @@
-I made 2 labs
-
-1st lab -> Measure volatages percentage
-2st lab -> Measure temperature values
--- [Unit.9] Timers & ADC/WatchDog_Driver [ ATmega32 ]/AVR_Project2_Codes/Debug/AVR_MCAL/Timer_8bits_Driver/timer.d
+++
@@ -1,5 +0,0 @@
-AVR_MCAL/Timer_8bits_Driver/timer.o AVR_MCAL/Timer_8bits_Driver/timer.o:  \
- ../AVR_MCAL/Timer_8bits_Driver/timer.c \
-  C:\Users\Dell\eclipse-workspace\AVR_Project2\AVR_MCAL\inc/timer.h
-
-C:\Users\Dell\eclipse-workspace\AVR_Project2\AVR_MCAL\inc/timer.h:
